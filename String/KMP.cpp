07396aa1740fb5f5b0b3661ea76ef73b7c74b89d
shahnewaz-labib <63904486+shahnewaz-labib@users.noreply.github.com>
2021-08-03 01:54:15 +0600

    updated KMP

--- String/KMP.cpp
+++ String/KMP.cpp
@@ -4,7 +4,32 @@
 //colab between labib and faisal
 #include <bits/stdc++.h>
 using namespace std;
+/*
+void kmp(string &text,string &pattern){
+	
+	int n=pattern.length(),l=text.length(),i=0,j,lps[n];
+	lps[0]=0;
+	for(j=1;j<n;){
+		if(pattern[i]==pattern[j]) lps[j]=i+1,i++,j++;
+		else {
+			if(i) i=lps[i-1];
+			else lps[j]=0,j++;
+		}
+	}
 
+	i=0,j=0;
+	while(i<n){
+		if(pattern[j]==text[i])
+			i++,j++;
+		else {
+			if(j) j=lps[j-1];
+			else i++;
+		}
+	}
+}
+
+
+*/
 vector<int> lpsGen(string pattern) {
     int len = pattern.size();
     vector<int> lps(len);
